6c1d887fc5be0a336112627e00440653942ee9b8
Venkatesan-dharmaraj <venkatesandmca@gmail.com>
2021-02-23 21:26:33 +0530

    backend changes committed

---
+++ klm/src/main/java/klm/ticketbooking/KlmApplication.java
@@ -0,0 +1,13 @@
+package klm.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlmApplication.class, args);
+	}
+
+}
